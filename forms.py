56e8ef0f1180753aef8dbe2e4475cf5972fcf2a6
Vladislavkn <inova136@gmail.com>
2020-12-26 22:25:41 +0300

    add user form

--- forms.py
+++ forms.py
@@ -6,3 +6,8 @@ class SourceForm(FlaskForm):
     title = StringField("Title: ", validators=[DataRequired()], render_kw={"placeholder": "Title"})
     url = StringField("URL: ", validators=[DataRequired()], render_kw={"placeholder": "URL"})
     submit = SubmitField("Save")
+
+class UserForm(FlaskForm):
+    name = StringField('Name: ', validators=[DataRequired()], render_kw={'placeholder': 'Name'})
+    password = StringField('Password: ', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
+    submit = SubmitField('Submit')
